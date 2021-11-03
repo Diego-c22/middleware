bb00f09f5b0c61a8c940d93a0db1007c62193cc6
Diego <Diegocortes079@gmail.com>
2021-11-02 18:27:06 -0600

    Create sold items resources and sold items detail resources

---
+++ store/solditems_resources.py
@@ -0,0 +1,71 @@
+"""Resources to connect with sold items model"""
+from flask import Blueprint
+from db.db import DataBase
+from flask_restful import Resource, Api, abort, reqparse
+
+solditems_v1 = Blueprint("solditems_v1", __name__)
+api = Api(solditems_v1)
+
+
+class SoldItemsResource(Resource):
+    def get(self):
+        try:
+            db = DataBase()
+            response = db.select_all("Articuloscomprados")
+            print(response)
+            return response, 200
+        except Exception as e:
+            raise e
+            abort(404, message="No se encontraron elementos")
+
+    def post(self):
+        args = arguments.parse_args()
+        db = DataBase()
+        response = db.insert_element("Articuloscomprados", **args)
+        return response, 200
+
+
+arguments = reqparse.RequestParser()
+arguments.add_argument('IdCompra', type=int,
+                       help="Este campo es obligatorio", required=True)
+arguments.add_argument(
+    'IdArticulo', type=int, help="Este campo es obligatorio", required=True)
+arguments.add_argument(
+    'Cantidad', type=int, help="Este campo es obligatorio", required=True)
+
+
+api.add_resource(SoldItemsResource, "/middleware/tienda/articulos_comprados/")
+
+
+class SoldItemsResourceDetail(Resource):
+    def get(self, id):
+        db = DataBase()
+        response = db.select_detail(
+            "Articuloscomprados", "IdArticuloComprado", id)
+        return response, 200
+
+    def patch(self, id):
+        args = arguments_update.parse_args()
+        db = DataBase()
+        response = db.update_element(
+            "Articuloscomprados", "IdArticuloComprado", id, **args)
+        return response, 200
+
+    def delete(self, id):
+        db = DataBase()
+        response = db.delete_element(
+            "Articuloscomprados", "IdArticuloComprado", id)
+        return response, 200
+
+
+arguments_update = reqparse.RequestParser()
+arguments_update.add_argument('IdCompra', type=int,
+                              help="Este campo es obligatorio", required=False)
+arguments_update.add_argument(
+    'IdArticulo', type=int, help="Este campo es obligatorio", required=False)
+arguments_update.add_argument(
+    'Cantidad', type=int, help="Este campo es obligatorio", required=False)
+
+
+api.add_resource(SoldItemsResourceDetail,
+                 "/middleware/tienda/articulos_comprados/<int:id>")
